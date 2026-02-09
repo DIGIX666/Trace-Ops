e21146428db924d5a210ffac7aed620aa6f64b32
L-PL <louispeyroul@gmail.com>
2026-02-10 00:15:14 +0100

    Modif Home.vue

--- zone1/services/frontend-vue/src/views/Home.vue
+++ zone1/services/frontend-vue/src/views/Home.vue
@@ -4,6 +4,71 @@
 <template>
   <div class="home">
     <h1>Choisir une section</h1>
-    <div class="menu"></div>
+    <div class="menu">
+      <div class="menu-card" v-if="hasRole('operateur')"><RouterLink to="/alert">Terrain (Injection)</RouterLink></div>
+      <div class="menu-card" v-if="hasRole('analyste')"><RouterLink to="/j2">J2 (Analyse)</RouterLink></div>
+      <div class="menu-card" v-if="hasRole('decideur')"><RouterLink to="/em">EM (Décision)</RouterLink></div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { inject, onMounted, ref } from "vue";
+const keycloak = window.__KEYCLOAK
+const userRoles = ref([]);
+
+const navRoles = ['operateur','analyste','decideur'];
+
+onMounted(() => {
+  if (keycloak?.authenticated) {
+    userRoles.value = keycloak.tokenParsed?.realm_access?.roles || [];
+    console.log(userRoles);
+    console.log("Nombre de rôles :", userRoles.value.length);
+    console.log(userRoles.value.filter(item => navRoles.includes(item)).length)
+  }
+});
+
+const hasRole = (role) => userRoles.value.includes(role);
+
+</script>
+
+<style>
+
+.menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px 20px;
+}
+
+.menu a {
+  color: black;
+  text-decoration: none;
+  display: block;
+  line-height: 100px;
+  text-align: center;
+}
+
+.menu-card {
+  position: relative;
+  width: 100%;
+  height: 100px;
+  border: 1px solid;
+}
+
+.menu-card a::after {
+  content: "→";
+  position: absolute;
+  bottom: 10px;
+  right: 30px;
+  font-size: 20px;
+  color: gray;
+  transition: transform 0.2s ease;
+}
+
+.menu-card a:hover::after {
+  transform: translateX(5px);
+  color: black;
+}
+
+</style>
